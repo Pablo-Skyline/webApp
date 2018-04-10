a4f923cfce0935fe091622d2275f2fab5152688a
skylineBot <skylineBot@github.com>
2018-04-09 19:52:03 -0500

    Hello World
    Práctica de  aprendizaje con Hello World

---
+++ Hello_World_Demo/app/app.component.ts
@@ -0,0 +1,11 @@
+import {Component, OnInit} from '@angular/core';
+@Component({
+   template: `Hello World`,
+    selector: 'my-app'
+})
+
+
+export class AppComponent {
+ constructor() {
+        }   
+}
